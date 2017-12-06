a1ebbec04f35de071686b7f2c06253c9f0504ede
yaonote <liscer@163.com>
2017-12-06 18:02:59 +0800

    获取用户信息，以及底部tabBar图标的更改

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,4 +1,20 @@
-
+var app = getApp();
 Page({
- 
+     data:{
+       wisdoms: {
+         wisdom: "如果永远不去做你能力以外的事，你就永远无法进步。",
+         title: "功夫熊猫3"
+       },
+       userInfo:{}
+     },
+     onLoad: function(){
+        var that = this;
+        app.getUserInfo(function(userInfo){
+            that.setData(
+              {
+                userInfo: userInfo
+              }
+            )
+        })
+     }
 })
